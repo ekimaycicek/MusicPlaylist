84d304f85ea6a601237dcf19c78a1c3231a22cae
ekimaycicek <ekimaycicek@icloud.com>
2023-10-20 23:06:32 +0300

    new

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -32,7 +32,6 @@ public class Playlist {
 
 
 
-
             if (vote == 1) {
                 System.out.print("Enter the song you want to add: ");
                 String song = input.nextLine();
